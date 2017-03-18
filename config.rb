09bb0c6c336fc3036df8fc0ed807628452627421
collinsnji <collins.geek@gmail.com>
2017-03-18 15:34:21 +0100

    Add application layer and UI

---
+++ config.rb
@@ -0,0 +1,9 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "userinterface/stylesheet"
+sass_dir = "userinterface/scss"
+javascripts_dir = "userinterface/javascript"
+
+line_comments = false
---
+++ main.js
@@ -0,0 +1,27 @@
+const electron = require('electron');
+const path = require('path');
+const ipcMain = require('electron').ipcMain;
+
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+
+var medkitHome = path.join('file://', __dirname, 'userinterface/pages/index.html');
+var destroyWindow = () => { mainWindow = null; }
+var activateWindowDarwin = () => { if (mainWindow = null) createWindow; }
+var closeAllWindow = () => { if (process.platform !== 'darwin') app.quit(); }
+
+var createWindow = function () {
+	mainWindow = new BrowserWindow({
+		width: 1024,
+		height: 600,
+		title: 'MedKit - Symptoms Search Engine',
+		titleBarStyle: 'hidden'
+	});
+	mainWindow.loadURL(medkitHome);
+	mainWindow.on('closed', destroyWindow);
+};
+
+app.on('ready', createWindow);
+app.on('window-all-closed', closeAllWindow);
